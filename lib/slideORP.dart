a976924300ca8038d07c55497935cb72e8127cf7
josdcarr <84652034+josdcarr@users.noreply.github.com>
2024-02-04 07:46:48 -0500

    Update slideORP.dart

--- lib/slideORP.dart
+++ lib/slideORP.dart
@@ -35,7 +35,7 @@ class _SliderVerticalWidgetState extends State<SliderORPWidget> {
 
         trackShape: RectangularSliderTrackShape(),
 
-        /// ticks in between
+        
         activeTickMarkColor: Colors.transparent,
         inactiveTickMarkColor: Colors.transparent,
       ),
