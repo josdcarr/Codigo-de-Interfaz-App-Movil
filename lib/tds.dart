ec228bba6b4c921a4c18a0522e361bc75ed280d6
josdcarr <84652034+josdcarr@users.noreply.github.com>
2024-02-04 07:48:03 -0500

    Update tds.dart

--- lib/tds.dart
+++ lib/tds.dart
@@ -18,7 +18,6 @@ class OpcionTDSState extends State<OpcionTDS> {
   void initState() {
     super.initState();
 
-    // Attach a real-time listener to fetch and update user1's data
     referencia.onValue.listen((event) {
       if (event.snapshot.value != null) {
         setState(() {
